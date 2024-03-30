5ba27397ce086597d195cc50fd763a775f089f39
jjokkorr <miha.0000@icloud.com>
2024-03-30 21:36:15 +0300

    first commit

--- Test.py
+++ Test.py
@@ -1,20 +1,19 @@
 # Основаная функция выполняет вычислаения (умножение, деление, вычитание, сложение)  
 # и возвращает результаты вычислений в виде целого числа.
 
-def main(string): 
-    if len(string.replace(" ", "")) > 1 and len(string.replace(" ", "")) < 4: # Обработка ошибок 
-        try: 
-           result = int(eval(string)) # функция eval выполняет вычисляемые операции.
-           print(string,"=", result, sep=" ") # вывод результата
-        
-        except: # Обработка ошибок
-           print('throws Exception') # вывод сообщения об ошибке
-    else: 
-        print('throws Exception') # вывод сообщения об ошибке
+def main(string):
+    # Обработка ошибок и проверка строки
+    if len(string.replace(" ", "")) > 1 and len(string.replace(" ", "")) < 5:
+        try:
+            result = int(eval(string))  # Функция eval вычисляет выражение в строке.
+            output = f"{string} = {result}"  # Формируем строку с примером и ответом
+            return output # Возвращаем результат
+        except:  # Обработка ошибок
+            return 'throws Exception'  # Сообщение об ошибке
+    else:
+        return 'throws Exception'  # Сообщение об ошибке
 
 # Начало выполнения программы. Принимаем на вход строку, которая содержит вычисляемые операции.
-
-if __name__ == '__main__':  
-    main(input())       
-    
-
+if __name__ == '__main__':
+    result = main(input())
+    print(result)
