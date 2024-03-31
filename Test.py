cedda70f6d45f0a75b61d43ea947473d8726f764
jjokkorr <miha.0000@icloud.com>
2024-03-31 14:41:17 +0300

    commit

--- Test.py
+++ Test.py
@@ -1,9 +1,13 @@
 # Основаная функция выполняет вычислаения (умножение, деление, вычитание, сложение)  
 # и возвращает результаты вычислений в виде целого числа.
-
-def main(string):
-    # Обработка ошибок и проверка строки
-    if len(string.replace(" ", "")) > 1 and len(string.replace(" ", "")) < 5:
+import re
+def main(numbs):
+    string = ""
+    for i in numbs:
+        string += str(i)
+        string += " "
+    dost = re.findall(r"\d+", string)
+    if len(string.replace(" ", "")) > 1 and len(string.replace(" ", "")) < 5 and int(dost[0]) < 11 and int(dost[1]) < 11:
         try:
             result = int(eval(string))  # Функция eval вычисляет выражение в строке.
             output = f"{string} = {result}"  # Формируем строку с примером и ответом
@@ -13,7 +17,10 @@ def main(string):
     else:
         return 'throws Exception'  # Сообщение об ошибке
 
-# Начало выполнения программы. Принимаем на вход строку, которая содержит вычисляемые операции.
+
+
+
 if __name__ == '__main__':
-    result = main(input())
+    numbs = [x for x in input().split(" ")]
+    result = main(numbs)
     print(result)
